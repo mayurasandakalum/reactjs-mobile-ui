c6005017b51853a5d358dbeeb86abff319ec9ce0
mayurasandakalum <mssellapperuma@gmail.com>
2023-05-04 16:54:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LiveLocation from "./screens/LiveLocation";
+
+const App = () => {
+  return (
+    <div>
+      <LiveLocation />
+    </div>
+  );
+};
+
+export default App;
